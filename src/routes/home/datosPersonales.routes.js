ff8fa1a9ea6a4876583df5afee76c3d8036cd8ef
Alejandro <161656941+AlejandroH-H@users.noreply.github.com>
2024-06-26 02:19:42 -0400

    Arreglo de Bugs e indentación

--- src/routes/home/datosPersonales.routes.js
+++ src/routes/home/datosPersonales.routes.js
@@ -2,22 +2,24 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/estudiante/datos',(req,res)=>{
-
-    console.log(req.session.name);
-    console.log(req.session.lastname);
-    if(req.session.loggedin == true) {
-        res.render('pages/principalHome/datos', {
-          login: true,
-          name: req.session.name,
-          lastname: req.session.lastname,
-          age: req.session.age,
-          email: req.session.email,
-          cedula: req.session.cedula,
-        });
-      }else{
-        res.redirect('/');
-      }
+router.get('/estudiante/datos', (req, res) => {
+  if (req.session.loggedin == true) {
+    res.render('pages/principalHome/datos', {
+      login: true,
+      name: req.session.name,
+      lastname: req.session.lastname,
+      age: req.session.age,
+      email: req.session.email,
+      cedula: req.session.cedula,
+    });
+  }else if (req.session.Adminloggedin == true) {
+    res.render('pages/administrador/administradorHome', {
+        login: true,
+        AdminName: req.session.adminName
+    });
+  }else{
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
